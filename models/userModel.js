55a03be0782aad6dc2b0ec5d2ada6d9d0790742b
bobsmoke05 <bobhyvakka@gmail.com>
2026-02-02 15:52:04 +0200

    feat(models): add userModel deleteOneById()

--- models/userModel.js
+++ models/userModel.js
@@ -23,9 +23,39 @@ const findById = (id) => {
     } else return false;
 } 
 
+const updateOneById = (id, updatedData) => {  const user = findById(id);
+  if (user) {
+    if (updatedData.name) {
+      user.name = updatedData.name;
+    }
+    if (updatedData.info) {
+      user.info = updatedData.info;
+    }
+    if (updatedData.image) {
+      user.image = updatedData.image;
+    }
+    if (updatedData.price) {
+      user.price = updatedData.price;
+    }
+    return user;
+  }
+  return false;
+};
+
+const deleteOneById = (id) => {
+  const user = findById(id);
+  if (user) {
+    const initialLength = userArray.length;
+    userArray = userArray.filter((user) => user.id !== Number(id));
+    return userArray.length < initialLength;
+  } else return false;
+};
+
 module.exports = {
     getAll,
     addOne,
     findById,
+    updateOneById,
+    deleteOneById,
 };
 
